cf237eefc8683643daa669692cefceaa761168f0
Reece Williams <reecepbcups@gmail.com>
2023-12-15 22:45:24 -0600

    tasks

--- src/error.rs
+++ src/error.rs
@@ -8,4 +8,13 @@ pub enum Never {}
 pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
+
+    #[error("Not enough funds, required: {required}")]
+    NotEnoughFunds { required: String },
+
+    #[error("Unauthorized")]
+    Unauthorized {},
+
+    #[error("Website name '{name}' already exists")]
+    AlreadyExists { name: String },
 }
--- src/msg.rs
+++ src/msg.rs
@@ -1,21 +1,35 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::Coin;
 
 #[cw_serde]
-pub struct InstantiateMsg {}
+pub struct InstantiateMsg {
+    pub managers: Option<Vec<String>>,
+
+    pub cost: Option<Coin>,
+
+    /// per_period is the amount of blocks that the cost is valid for
+    /// if None, the site is hosted forever at the original cost (initialization fee)
+    /// if per_period is Some(), the site cost is required for every per_period blocks
+    ///
+    /// i.e. if per_period is Some(100), the site cost is required every 100 blocks. Preload your account with funds
+    pub period: Option<u64>,
+
+    /// false for non x/clock module chains (SudoMsg).
+    pub using_clock: bool,
+}
 
 #[cw_serde]
 pub enum ExecuteMsg {
-    NewWebsite {
-        name: String,
-        source: String,
-    },
+    NewWebsite { name: String, source: String },
+    ExpireOld {},
+
+    // admin only
+    Remove { name: String },
 }
 
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryMsg {
     #[returns(crate::state::Website)]
-    GetWebsite {
-        name: String,
-    },
+    GetWebsite { name: String },
 }
